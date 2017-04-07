5ee1b837a5b5e427137c4d72ff1e5a51f21846c1
Sean Martin <srmartin777@gmail.com>
2017-04-07 14:37:58 -0400

    adusted skills layout

--- public/js/projectAutoLingo.js
+++ public/js/projectAutoLingo.js
@@ -13,13 +13,7 @@
   var autoLingoText = [];
 
   autoLingoText.push(
-    'AutoLingo is a web application that allows multiple users speaking different languages to communicate with each other in real time. All a user needs to do is select their desired language and enter a Group Chatroom or start a 1-on-1 Video Chat.'
-  );
-  autoLingoText.push(
-    'Group Chat - Multiple users can enter a chat room and speak any language they select. Any messages received in a different language, will be translated into the user’s selected language, then displayed on their own unique conversation log.'
-  );
-  autoLingoText.push(
-    'Video Chat - Two users can speak with each other via an audio/video display. Each user’s words are transcribed into text, translated into the other user’s language, and then rendered on each user’s screen in the language they selected.'
+    'AutoLingo is a web application that allows multiple users speaking different languages to communicate with each other in real time. Multiple users can chat to each other in different languages via a text-only chat room, and all messages are rendered on the user’s screen in their selected language. Users can also start a one-on-one video/audio chat. Each user’s spoken words are transcribed into text, translated, then displayed on the message log in the languageeach user’s screen in the language they selected.'
   );
   autoLingoText.push(
     'Built with: React.js, Redux, Express.js, Node.js and Socket.io.'
