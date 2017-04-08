fa9bd9bf035c779e86a6bdc1fc35ea7864de34d5
Sean Martin <srmartin777@gmail.com>
2017-04-07 21:17:01 -0400

    made website responsive

--- public/js/projectAutoLingo.js
+++ public/js/projectAutoLingo.js
@@ -13,7 +13,7 @@
   var autoLingoText = [];
 
   autoLingoText.push(
-    'AutoLingo is a web application that allows multiple users speaking different languages to communicate with each other in real time. Multiple users can chat to each other in different languages via a text-only chat room, and all messages are rendered on the user’s screen in their selected language. Users can also start a one-on-one video/audio chat. Each user’s spoken words are transcribed into text, translated, then displayed on the message log in the languageeach user’s screen in the language they selected.'
+    'AutoLingo is a real-time translation web application, that allows users of different languages to communicate with each other. The video chat features live speech-to-text translations and the group chat features live text-only translation.'
   );
   autoLingoText.push(
     'Built with: React.js, Redux, Express.js, Node.js and Socket.io.'
