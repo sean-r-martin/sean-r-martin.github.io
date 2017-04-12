a61d385e254e6df296b7063f9404a66683971a4e
Sean Martin <srmartin777@gmail.com>
2017-04-12 12:27:31 -0400

    made fixed responsive issue on project section

--- public/js/projectNavi.js
+++ public/js/projectNavi.js
@@ -13,10 +13,13 @@
   var naviText = [];
 
   naviText.push(
-    'Web app for finding upcoming events around NYC. Event information is dynamically generated and integrated with Google Maps.'
+    'Navi is a web application for finding information about upcoming events around New York City. When a user logs onto the website, current event information is queried from various APIs, and rendered on the events list. Event information is also integrated with the Google Maps API, so indicators are shown on a google map to indicate each event location.'
   );
   naviText.push(
     'Built with: React.js, Redux, Express.js, and Node.js.'
+  );
+  naviText.push(
+    'Event Information APIs used: Eventful.com, Meetup.com, NYC Developer Portal.'
   );
 
   naviText.forEach(function (text) {
