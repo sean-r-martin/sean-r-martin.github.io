1c250a24c9734b158b5cd183737e524853cddaf9
Sean Martin <sean-r-martin@users.noreply.github.com>
2017-05-27 11:28:21 -0400

    Update projectPrimeRobotics.js

--- js/projectPrimeRobotics.js
+++ js/projectPrimeRobotics.js
@@ -6,7 +6,7 @@ $(document).ready(function createPrimeRoboticsDescription() {
   var primeRoboticsText = [];
 
   primeRoboticsText.push(
-    "A mock landing page for a technlogoy company. This project was created to practice styling web pages with HTML and CSS."
+    "A mock landing page for a technology company. This project was created to practice styling web pages with HTML and CSS."
   );
   primeRoboticsText.push(
     'Built with - HTML5, CSS3, Flexbox and jQuery.'
