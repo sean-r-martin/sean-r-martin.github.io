4739f3e1c6e802b3c2b85021e658644c44e1cbea
Sean Martin <srmartin777@gmail.com>
2020-06-20 16:31:27 -0400

    edit story

--- js/story.js
+++ js/story.js
@@ -11,11 +11,11 @@ $(document).ready(function createNaviDescription() {
   var storyText = [];
 
   storyText.push(
-    "Hi I'm Sean Martin! I'm a software engineer who loves solving problems through code and creating interesting applications. I have the skills to work with both frontend and backend technologies and I value a Test Driven Development style."
+    "Hi, I'm Sean Martin! I'm a software engineer who loves solving problems through code and creating interesting applications. I have the skills to work with both frontend and backend technologies and I value a Test Driven Development style."
   );
   
   storyText.push(
-    'In my free time, I work on side projects, play in a kickball league, and ride my bike around NYC.'
+    'In my free time I work on side projects, play in a kickball league, and ride my bike around NYC.'
   );
 
   storyText.forEach(function (text) {
